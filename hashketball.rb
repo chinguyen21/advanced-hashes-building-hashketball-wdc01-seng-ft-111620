4ff2e730e766ea10b3e991c5291ddf9ecc237ffb
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-27 13:52:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,13 @@
 # Write your code here!
 game_hash = {
   :home => {
-    
+    :team_name => "",
+    :colors => [],
+    :players => [
+      {
+        
+      }
+      ]
   },
   :away => {
     
